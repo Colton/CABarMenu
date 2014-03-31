a75fc52de7c3bde46f1a38c2bbf9378c9e81f6d4
icoltonanglin <hi@imcolton.me>
2014-03-31 02:29:00 -0500

    new

---
+++ Classes/CABarMenu.m
@@ -0,0 +1,102 @@
+
+
+#import "CABarMenu.h"
+
+#define kAnimationDuration 0.3
+
+@implementation CABarMenu {
+	UINavigationBar *parentBar;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+		    }
+    return self;
+}
+
+- (id)init
+{
+    //Menu Size
+    self = [self initWithFrame:CGRectMake(0, 0, 0, 300)];
+    return self;
+}
+
+
+
+//Final Frame Of The Animation
+- (CGRect)finalFrameForNavigationBar:(UINavigationBar *)bar {
+	CGRect rect = CGRectMake(37,
+							 bar.frame.origin.y + 1,
+							250,
+							 self.frame.size.height);
+	return rect;
+}
+
+- (CGRect)initialFrameForNavigationBar:(UINavigationBar *)bar {
+	CGRect rect = [self finalFrameForNavigationBar:bar];
+	rect.origin.y -= rect.size.height;
+	return rect;
+}
+
+- (void)showFromNavigationBar:(UINavigationBar *)bar animated:(BOOL)animated {
+	
+	parentBar = bar;
+	if (!parentBar) {
+		return;
+	}
+	
+	[bar.superview insertSubview:self belowSubview:bar];
+	
+	if (animated) {
+		self.frame = [self initialFrameForNavigationBar:bar];
+	}
+	
+	
+	
+	void (^animations)() = ^void() {
+		self.frame = [self finalFrameForNavigationBar:bar];
+		
+	};
+	
+	void (^completion)(BOOL) = ^void(BOOL finished) {
+		_visible = YES;
+	};
+	
+	if (animated) {
+		[UIView animateWithDuration:kAnimationDuration animations:animations completion:completion];
+	} else {
+		animations();
+		completion(YES);
+	}
+	
+}
+
+- (void)hideAnimated:(BOOL)animated {
+	if (!parentBar) {
+		return;
+	}
+	
+
+	void (^animations)() = ^void() {
+		self.frame = [self initialFrameForNavigationBar:parentBar];
+	
+		
+	};
+	
+	void (^completion)(BOOL) = ^void(BOOL finished) {
+		_visible = NO;
+		[self removeFromSuperview];
+	};
+	
+	if (animated) {
+		[UIView animateWithDuration:kAnimationDuration animations:animations completion:completion];
+	} else {
+		animations();
+		completion(YES);
+	}
+}
+
+@end
--- README.markdown
+++ README.markdown
@@ -7,3 +7,20 @@ A Tweetbot 3-like drop down menu (like the tweetbot list view by holding down th
 
 CABarMenu's content supports any kind of subview class! In the example, I use a table view. Images, collection views, buttons, and text fields also
 work with this.
+
+Usage
+=========
+
+1. Simply import all files from the ```Classes``` folder in to your project.
+2. In your desired view, import **CABarMenu.h**
+3. Define a CABarMenu in your implementation. ```	CABarMenu *menu;
+```
+4. In *super viewDidLoad*, call ```menu = [[CABarMenu alloc] init];
+```
+5. Make a button or something that shows/hides the menu. To show the menu,
+you can use: ```[menu showFromNavigationBar:navbarnamehere animated:YES];```
+And to hide the menu, ```[menu hideAnimated:YES];```.
+
+
+
+  
